8093d57a5d7d6231db1322e689462c808ea6930a
Furkan Kayapınar <furkankayapinar@Furkan-MacBook-Air.local>
2024-11-14 20:30:55 +0300

    fdle

---
+++ app.js
@@ -0,0 +1,18 @@
+/* 
+!direkt sayfaya çıktı verir
+document.writeln("Hello, World!");
+document.writeln("Hello, World222!");
+
+!Konsola çıktı verir geliştirici için kullanışlıdır
+console.log("Hello, World!");
+console.log(1234);
+console.log(true);
+console.log([1, 2, 3, 4]);
+console.log({ name: "Hakan", age: 25 });
+console.log(window);
+console.log(window.document.writeln("Hello, World!"));
+
+!popup mesajı verir
+alert("this is an alert message");
+*/
+console.log(window.document.head.title);
