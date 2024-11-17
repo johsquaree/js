2eaaef4bd27e38b5476eb0c60a69ac2df68c4f74
Furkan Kayapınar <furkankayapinar@Furkan-MacBook-Air.local>
2024-11-17 22:01:56 +0300

    docfix

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 // !direkt sayfaya çıktı verir
-// document.writeln("Hello, World!");
-// document.writeln("Hello, World222!");
+document.writeln("Hello, World!");
+document.writeln("Hello, World222!");
 
 // !Konsola çıktı verir geliştirici için kullanışlıdır
-// console.log("Hello, World!");
-// console.log(1234);
-// console.log(true);
-// console.log([1, 2, 3, 4]);
-// console.log({ name: "Hakan", age: 25 });
-// console.log(window);
-// console.log(window.document.writeln("Hello, World!"));
-// console.log(window.document.head.title);
+console.log("Hello, World!");
+console.log(1234);
+console.log(true);
+console.log([1, 2, 3, 4]);
+console.log({ name: "Hakan", age: 25 });
+console.log(window);
+console.log(window.document.writeln("Hello, World!"));
+console.log(window.document.head.title);
 
 // !popup mesajı verir
-// alert("this is an alert message");
+alert("this is an alert message");
 
 // !Kullanıcıdan veri alma
-// let isim1=prompt("Enter your name: "); // !prompt sadece string döner number almak istiyorsak type conversion yapmalıyız
-// let isim2=prompt("Enter your name2: ");
-// window.prompt("Enter your name2: ");
-// console.log(prompt("Enter your name3: "));
-// console.log(window.prompt("Enter your name4: "));
+let isim1=prompt("Enter your name: "); // !prompt sadece string döner number almak istiyorsak type conversion yapmalıyız
+let isim2=prompt("Enter your name2: ");
+window.prompt("Enter your name2: ");
+console.log(prompt("Enter your name3: "));
+console.log(window.prompt("Enter your name4: "));
 
-// console.log("ismin bu bence: " +isim1);
+console.log("ismin bu bence: " +isim1);
 
-// let booluen = confirm("Are you sure?");
-// console.log(booluen);
-// if (booluen == true) {
-//     booluen = 1;}
-// else { booluen = false;
-//     booluen = 0;}
+let booluen = confirm("Are you sure?");
+console.log(booluen);
+if (booluen == true) {
+    booluen = 1;}
+else { booluen = false;
+    booluen = 0;}
 
-// let numberBoolean = Number(booluen);
-// console.log(numberBoolean);
+let numberBoolean = Number(booluen);
+console.log(numberBoolean);
 let vize1 = prompt("Enter your vize1: ");
 let vize2 = prompt("Enter your vize2: ");
 let final = prompt("Enter your final: ");
